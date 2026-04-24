fd4ae8642d5403be4781050a87d00055a32dad0c
Cameron Campbell <cameron.campbell325@gmail.com>
2026-04-24 16:31:51 +0100

    updated tweens docs

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,144 +1,163 @@
-import * as fs from "fs"
-import { defineConfig } from 'vitepress'
+import * as fs from "fs";
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
-    // site-level options
-    title: 'Rsml',
-    description: 'a declarative styling language for the Roblox ecosystem.',
-    srcDir: './src',
-    cleanUrls: true,
+  // site-level options
+  title: "Rsml",
+  description: "a declarative styling language for the Roblox ecosystem.",
+  srcDir: "./src",
+  cleanUrls: true,
 
-    head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
 
-    markdown: {
-        languages: [
-            JSON.parse(fs.readFileSync('./.vitepress/syntaxes/rsml.tmLanguage.json', 'utf8')),
+  markdown: {
+    languages: [
+      JSON.parse(
+        fs.readFileSync("./.vitepress/syntaxes/rsml.tmLanguage.json", "utf8"),
+      ),
 
-            JSON.parse(fs.readFileSync('./.vitepress/syntaxes/rsml-types.tmLanguage.json', 'utf8')),
+      JSON.parse(
+        fs.readFileSync(
+          "./.vitepress/syntaxes/rsml-types.tmLanguage.json",
+          "utf8",
+        ),
+      ),
 
-            JSON.parse(fs.readFileSync('./.vitepress/syntaxes/luau.tmLanguage.json', 'utf8')),
+      JSON.parse(
+        fs.readFileSync("./.vitepress/syntaxes/luau.tmLanguage.json", "utf8"),
+      ),
 
-            {
-                injectTo: ["source.luau"],
-                embeddedLanguages: {
-                    ["meta.embedded.rsml"]: "rsml"
-                },
-                ...JSON.parse(fs.readFileSync('./.vitepress/syntaxes/rsml-luau.tmLanguage.json', 'utf8'))
-            },
+      {
+        injectTo: ["source.luau"],
+        embeddedLanguages: {
+          ["meta.embedded.rsml"]: "rsml",
+        },
+        ...JSON.parse(
+          fs.readFileSync(
+            "./.vitepress/syntaxes/rsml-luau.tmLanguage.json",
+            "utf8",
+          ),
+        ),
+      },
 
-            JSON.parse(fs.readFileSync('./.vitepress/syntaxes/ts.tmLanguage.json', 'utf8')),
+      JSON.parse(
+        fs.readFileSync("./.vitepress/syntaxes/ts.tmLanguage.json", "utf8"),
+      ),
 
-            {   
-                injectTo: ["source.ts"],
-                embeddedLanguages: {
-                    ["meta.embedded.rsml"]: "rsml"
-                },
-                ...JSON.parse(fs.readFileSync('./.vitepress/syntaxes/rsml-ts.tmLanguage.json', 'utf8'))
-            }
-        ],
-        theme: {
-            "name": "VSCode Dark",
-            "settings": [
-                {
-                    "scope": ["comment", "comment.block"],
-                    "settings": {
-                        "foreground": "#6A9955"
-                    }
-                },
+      {
+        injectTo: ["source.ts"],
+        embeddedLanguages: {
+          ["meta.embedded.rsml"]: "rsml",
+        },
+        ...JSON.parse(
+          fs.readFileSync(
+            "./.vitepress/syntaxes/rsml-ts.tmLanguage.json",
+            "utf8",
+          ),
+        ),
+      },
+    ],
+    theme: {
+      name: "VSCode Dark",
+      settings: [
+        {
+          scope: ["comment", "comment.block"],
+          settings: {
+            foreground: "#6A9955",
+          },
+        },
 
-                {
-                    "scope": ["string"],
-                    "settings": {
-                        "foreground": "#ce9178"
-                    }
-                },
+        {
+          scope: ["string"],
+          settings: {
+            foreground: "#ce9178",
+          },
+        },
 
-                {
-                    "scope": ["variable"],
-                    "settings": {
-                        "foreground": "#9CDCFE"
-                    }
-                },
+        {
+          scope: ["variable"],
+          settings: {
+            foreground: "#9CDCFE",
+          },
+        },
 
-                {
-                    "scope": ["constant.numeric"],
-                    "settings": {
-                        "foreground": "#b5cea8"
-                    }
-                },
+        {
+          scope: ["constant.numeric"],
+          settings: {
+            foreground: "#b5cea8",
+          },
+        },
 
-                {
-                    "scope": ["support.class", "entity.name.type.enum"],
-                    "settings": {
-                        "foreground": "#4EC9B0"
-                    }
-                },
+        {
+          scope: ["support.class", "entity.name.type.enum"],
+          settings: {
+            foreground: "#4EC9B0",
+          },
+        },
 
-                {
-                    "scope": ["variable.other.constant"],
-                    "settings": {
-                        "foreground": "#4FC1FF"
-                    }
-                },
+        {
+          scope: ["variable.other.constant"],
+          settings: {
+            foreground: "#4FC1FF",
+          },
+        },
 
-                {
-                    "scope": ["keyword.control"],
-                    "settings": {
-                        "foreground": "#C586C0"
-                    }
-                },
+        {
+          scope: ["keyword.control"],
+          settings: {
+            foreground: "#C586C0",
+          },
+        },
 
-                {
-                    "scope": ["entity.name.tag.css"],
-                    "settings": {
-                        "foreground": "#D7BA7D"
-                    }
-                },
+        {
+          scope: ["entity.name.tag.css"],
+          settings: {
+            foreground: "#D7BA7D",
+          },
+        },
 
+        {
+          scope: ["support.function", "entity.name.function"],
+          settings: {
+            foreground: "#D8D7A3",
+          },
+        },
 
-                {
-                    "scope": ["support.function", "entity.name.function"],
-                    "settings": {
-                        "foreground": "#D8D7A3"
-                    }
-                },
-
-                {
-                    "scope": ["storage.modifier", "constant.language", "storage.type"],
-                    "settings": {
-                        "foreground": "#4191CB"
-                    }
-                },
-            ]
-        }
+        {
+          scope: ["storage.modifier", "constant.language", "storage.type"],
+          settings: {
+            foreground: "#4191CB",
+          },
+        },
+      ],
     },
-  
-    themeConfig: {
-        outline: [ 1, 3 ],
-        socialLinks: [
-            { icon: 'github', link: 'https://github.com/rbx-rsml' },
-            { icon: 'discord', link: 'https://discord.gg/67CWCwNT6R' },
-        ],
-        sidebar: [
-            {
-                items: [
-                    { text: 'Introduction', link: '/intro' },
-                ]
-            },
+  },
 
-            {
-                text: "Docs",
-                items: [
-                    { text: "Selectors", link: "docs/selectors" },
-                    { text: "Fields", link: "docs/fields" },
-                    { text: "Derives", link: "docs/derives" },
-                    { text: "Names", link: "docs/names" },
-                    { text: "Priorities", link: "docs/priorities" },
-                    { text: "Data Types", link: "docs/datatypes" },
-                    { text: "Macros", link: "docs/macros" },
-                    { text: "Comments", link: "docs/comments" },
-                ]
-            }
-        ]
-    },
-}) 
+  themeConfig: {
+    outline: [1, 3],
+    socialLinks: [
+      { icon: "github", link: "https://github.com/rbx-rsml" },
+      { icon: "discord", link: "https://discord.gg/67CWCwNT6R" },
+    ],
+    sidebar: [
+      {
+        items: [{ text: "Introduction", link: "/intro" }],
+      },
+
+      {
+        text: "Docs",
+        items: [
+          { text: "Selectors", link: "docs/selectors" },
+          { text: "Fields", link: "docs/fields" },
+          { text: "Tweens", link: "docs/fields" },
+          { text: "Derives", link: "docs/derives" },
+          { text: "Names", link: "docs/names" },
+          { text: "Priorities", link: "docs/priorities" },
+          { text: "Data Types", link: "docs/datatypes" },
+          { text: "Macros", link: "docs/macros" },
+          { text: "Comments", link: "docs/comments" },
+        ],
+      },
+    ],
+  },
+});
--- src/docs/tweens.md
+++ src/docs/tweens.md
@@ -1,7 +1,9 @@
 # Tweens
 
 ::: warning
-Tweens are currently not implemented on Roblox Stylesheets yet.
+StyleSheet transitions are currently not available in live servers yet. You'll need the following fflags to enable them: `FFlagStylingTransitionsBetaFeature`, `FFlagStyleEditorSupportTransitions2`, `FFlagStyleEditorShowDefaultTransitions`, `FFlagStylingTransitionsDefaultTransition`.
+
+Rojo also doesn't support StyleSheet transitions yet. You'll need to use [this fork](https://github.com/rbx-rsml/rojo/releases/tag/v7.7.0-rc.1-stylesheets.0).
 :::
 
 Tweens let you apply a transition / animation to a StyleSheet property.
